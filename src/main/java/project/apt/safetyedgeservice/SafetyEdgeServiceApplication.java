d34e72ac33c0551b3011028e33f2f47d1cd5aa54
Albert Baffour Awuah <baffour006@gmail.com>
2021-10-03 12:38:47 +0200

    first commit

---
+++ src/main/java/project/apt/safetyedgeservice/SafetyEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package project.apt.safetyedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafetyEdgeServiceApplication.class, args);
+    }
+
+}
